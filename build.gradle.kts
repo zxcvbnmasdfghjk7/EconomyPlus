7e531c969c32e7d665b6c29e8a9b49b63b623e91
TheZachiePlayz123 <zhouedmund89@gmail.com>
2021-04-25 15:53:19 +1000

    Rewrite build script

---
+++ build.gradle.kts
@@ -0,0 +1,40 @@
+buildscript {
+    val shadowJar_Version by rootProject.extra{
+        "6.1.0"
+    }
+
+    val slf4j_Version by rootProject.extra{
+        "1.7.30"
+    }
+    repositories{
+        mavenCentral()
+        maven{
+            url = uri("https://plugins.gradle.org/m2/")
+        }
+    }
+
+    dependencies{
+        classpath("com.github.jengelman.gradle.plugins:shadow:${rootProject.extra.get("shadowJar_Version")}")
+    }
+}
+
+allprojects{
+    group = "io.github.eddiediamondfire"
+    version = "dev-SNAPSHOT"
+}
+
+subprojects{
+    repositories{
+        mavenCentral()
+    }
+
+    apply(plugin = "java")
+    apply(plugin = "idea")
+    apply(plugin = "com.github.johnrengelman.shadow")
+
+    val implementation by configurations
+    dependencies{
+        implementation("org.slf4j:slf4j-api:${rootProject.extra.get("slf4j_Version")}")
+        implementation("org.slf4j:slf4j-log4j12:${rootProject.extra.get("slf4j_Version")}")
+    }
+}
